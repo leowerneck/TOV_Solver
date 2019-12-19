5fa3244f8131fd583edda391f9714c65e7d9fcfb
Leonardo Werneck <leow155@gmail.com>
2019-12-19 09:53:42 -0500

    Added copyright notice

--- TOV_Solver.C
+++ TOV_Solver.C
@@ -1,3 +1,9 @@
+/* .-----------------------------------------.
+ * |  Copyright (c) 2019, Leonardo Werneck   |
+ * | Licensed under the BSD 2-Clause License |
+ * .-----------------------------------------.
+ */
+
 /* Program     : TOV Solver
  * File        : TOV_Solver.C
  * Author      : Leo Werneck (werneck@if.usp.br)
